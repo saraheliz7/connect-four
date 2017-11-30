b87ec8586b8e3a1757c7bc224fdd88435db4f6a7
saraheliz7 <woodsose@gmail.com>
2017-11-29 19:27:16 -0500

    Clicking top column now places a color in first available space from the bottom

--- board.js
+++ board.js
@@ -11,18 +11,18 @@ var count = 0;
 var gameOver = false;
 
 
-var showCellColor = function(row, column, value) {
+var showCellColor = function(row, column, color) {
   var cell = document.querySelector(".cell-" + row + "-" + column);
-  if(value === "red") {
+  if(color === "red") {
     cell.querySelector(".red").classList.remove("hidden-red");
-  } else if (value === "yellow") {
+  } else if (color === "yellow") {
     cell.querySelector(".yellow").classList.remove("hidden-yellow");
   }
 };
 
 var drawBoard = function(board){
   for(var row = 0; row < board.length; row++) {
-    for(var column = 0; column < board.length + 1; column++) {
+    for(var column = 0; column < board[row].length; column++) {
       showCellColor(row, column, board[row][column]);
     }
   }
@@ -36,28 +36,33 @@ var attachCellClickHandler = function(row, column, board, callback) {
 };
 
 var attachBoardClickHandlers = function(board, callback) {
-  for(var row = 0; row < board.length; row++) {
-    for(var column = 0; column < board.length + 1; column++) {
+  for(var row = 0; row < 1; row++) {
+    for(var column = 0; column < board[row].length; column++) {
       attachCellClickHandler(row, column, board, callback)
     }
   }
 }
 
 
-var setCellColor = function(board, row, column) {
-  if(count % 2 === 0) {
-    board[row][column] = "red";
-  } else {
-    board[row][column] = "yellow";
+var setCellColor = function(board, column) {
+  for(var row = board.length -1; row >= 0; row--) {
+    if(board[row][column] === "") {
+      if(count % 2 === 0) {
+        board[row][column] = "red";
+      } else {
+        board[row][column] = "yellow";
+      }
+      count++;
+      return;
+    }
   }
-  count++
 };
 
 
 
 var move = function(row, column, board) {
-  if(gameOver === false && board[row][column] === "") {
-    setCellColor(board, row, column);
+  if(gameOver === false) {
+    setCellColor(board, column);
     drawBoard(board);
   }
 };
