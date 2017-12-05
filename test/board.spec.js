49bc4d620dfccdea29b824eaa6c6cb8b3323b54f
saraheliz7 <woodsose@gmail.com>
2017-12-05 10:43:18 -0500

    Tested vertical win check and overall win check

--- test/board.spec.js
+++ test/board.spec.js
@@ -1,7 +1,6 @@
 describe("horizontalWin", function(){
 
   describe("returns true when four of the same color in the ", function(){
-
     it("bottom right of board", function(){
       var board = [
         ["", "", "", "", "", "", ""],
@@ -15,7 +14,6 @@ describe("horizontalWin", function(){
       var hasWin = horizontalWin(board, "red");
       expect(hasWin).to.be.true;
     });
-
     it("top right of board", function(){
       var board = [
         ["", "", "", "red", "red", "red", "red"],
@@ -29,7 +27,6 @@ describe("horizontalWin", function(){
       var hasWin = horizontalWin(board, "red");
       expect(hasWin).to.be.true;
     });
-
     it("top left of board", function(){
       var board = [
         ["red", "red", "red", "red", "", "", ""],
@@ -43,7 +40,6 @@ describe("horizontalWin", function(){
       var hasWin = horizontalWin(board, "red");
       expect(hasWin).to.be.true;
     });
-
     it("bottom left of board", function(){
       var board = [
         ["", "", "", "", "", "", ""],
@@ -57,7 +53,6 @@ describe("horizontalWin", function(){
       var hasWin = horizontalWin(board, "red");
       expect(hasWin).to.be.true;
     });
-
     it("middle of the board", function(){
       var board = [
         ["", "", "", "", "", "", ""],
@@ -71,7 +66,162 @@ describe("horizontalWin", function(){
       var hasWin = horizontalWin(board, "red");
       expect(hasWin).to.equal(true);
     });
-
   });
 
+  it("returns true when four red spaces filled in a row", function(){
+    var board = [
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "red", "red", "red", "red", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""]
+    ];
+
+    var hasWin = horizontalWin(board, "red");
+    expect(hasWin).to.be.true;
+  });
+  it("returns true when four yellow spaces filled in a row", function(){
+    var board = [
+      ["", "", "", "", "", "", ""],
+      ["", "yellow", "yellow", "yellow", "yellow", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""]
+    ];
+
+    var hasWin = horizontalWin(board, "yellow");
+    expect(hasWin).to.be.true;
+  });
+});
+
+describe("verticalWin", function(){
+  describe("returns true when four of the same color in the ", function(){
+    it("top left of the board", function(){
+      var board = [
+        ["red", "", "", "", "", "", ""],
+        ["red", "", "", "", "", "", ""],
+        ["red", "", "", "", "", "", ""],
+        ["red", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", ""]
+      ];
+      var hasWin = verticalWin(board, "red");
+      expect(hasWin).to.be.true;
+    });
+    it("bottom left of the board", function(){
+      var board = [
+        ["", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", ""],
+        ["red", "", "", "", "", "", ""],
+        ["red", "", "", "", "", "", ""],
+        ["red", "", "", "", "", "", ""],
+        ["red", "", "", "", "", "", ""]
+      ];
+      var hasWin = verticalWin(board, "red");
+      expect(hasWin).to.be.true;
+    });
+    it("top right of the board", function(){
+      var board = [
+        ["", "", "", "", "", "", "red"],
+        ["", "", "", "", "", "", "red"],
+        ["", "", "", "", "", "", "red"],
+        ["", "", "", "", "", "", "red"],
+        ["", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", ""]
+      ];
+      var hasWin = verticalWin(board, "red");
+      expect(hasWin).to.be.true;
+    });
+    it("bottom right of the board", function(){
+      var board = [
+        ["", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "red"],
+        ["", "", "", "", "", "", "red"],
+        ["", "", "", "", "", "", "red"],
+        ["", "", "", "", "", "", "red"]
+      ];
+      var hasWin = verticalWin(board, "red");
+      expect(hasWin).to.be.true;
+    });
+    it("middle of the board", function(){
+      var board = [
+        ["", "", "", "", "", "", ""],
+        ["", "", "", "red", "", "", ""],
+        ["", "", "", "red", "", "", ""],
+        ["", "", "", "red", "", "", ""],
+        ["", "", "", "red", "", "", ""],
+        ["", "", "", "", "", "", ""]
+      ];
+      var hasWin = verticalWin(board, "red");
+      expect(hasWin).to.be.true;
+    });
+    it("middle of right hand column of the board", function(){
+      var board = [
+        ["", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "red"],
+        ["", "", "", "", "", "", "red"],
+        ["", "", "", "", "", "", "red"],
+        ["", "", "", "", "", "", "red"],
+        ["", "", "", "", "", "", ""]
+      ];
+      var hasWin = verticalWin(board, "red");
+      expect(hasWin).to.be.true;
+    });
+  });
+  it("returns true when four red spaces filled in a column", function(){
+    var board = [
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "red"],
+      ["", "", "", "", "", "", "red"],
+      ["", "", "", "", "", "", "red"],
+      ["", "", "", "", "", "", "red"],
+      ["", "", "", "", "", "", ""]
+    ];
+    var hasWin = verticalWin(board, "red");
+    expect(hasWin).to.be.true;
+  });
+  it("returns true when four yellow spaces filled in a column", function(){
+    var board = [
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "yellow", "", ""],
+      ["", "", "", "", "yellow", "", ""],
+      ["", "", "", "", "yellow", "", ""],
+      ["", "", "", "", "yellow", "", ""],
+      ["", "", "", "", "", "", ""]
+    ];
+    var hasWin = verticalWin(board, "yellow");
+    expect(hasWin).to.be.true;
+  });
+});
+
+describe("hasWin", function(){
+  it("returns true when horizontalWin is true", function(){
+    var board = [
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "red", "red", "red", "red"]
+    ];
+
+    var hasHorizontalWin = hasWin(board, "red");
+    expect(hasHorizontalWin).to.be.true;
+  });
+  it("returns true when verticalWin is true", function(){
+    var board = [
+      ["", "red", "", "", "", "", ""],
+      ["", "red", "", "", "", "", ""],
+      ["", "red", "", "", "", "", ""],
+      ["", "red", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""]
+    ];
+
+    var hasVerticalWin = hasWin(board, "red");
+    expect(hasVerticalWin).to.be.true;
+  });
 });
